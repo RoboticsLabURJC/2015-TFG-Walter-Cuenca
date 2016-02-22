632ba07d5152e9c66c06c77b8b1ee9e8408bc7d8
waltercito <cuenca306@hotmail.com>
2016-02-22 21:58:05 +0100

    Update Carrito_Compra
    Se ha modificado el código para adaptar el funcionamientos al proposito
    que tiene este ejemplo.
    Se ha incluido la posibilidad de cargar imagenes desde el
    servidor(NodeJs)

--- Lado_Servidor/Cookies/Carrito_Compra/Server.js
+++ Lado_Servidor/Cookies/Carrito_Compra/Server.js
@@ -1,15 +1,15 @@
 
-var app = require('express')();
+var express=require('express');
+var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
 ///cargar imagenes de forma static ---> el path que se utiliza es /imagenes/[name_img]
-app.user(express.static('public'));
 
+app.use(express.static('public'));
 
-
-app.use(cookieParser());
+app.use(cookieParser()); //-->necesario para el parser el cookie 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
@@ -22,54 +22,37 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
 /////////////////////////////////////
 // Limpiar el valor de las cookies //
 /////////////////////////////////////
 
 app.get('/clear', function(req, res){
- 	res.clearCookie('luis');
  	res.clearCookie('walter');
-	res.clearCookie('juan');
+ 	res.clearCookie('manuel');
+	res.clearCookie('franja');
  	res.sendFile(__dirname + '/index.html');
 });
-
 
 /////////////////////////////////////////////
 // El usuario añade productos para comprar //
 ////////////////////////////////////////////
-
   
 app.post('/',urlencodedParser,function(req,res){
-  console.log("=== El usuario añade producto  ===");
-  for(elemento in req.body){
-  	contenido = elemento;
-  }
-  contenido = JSON.parse(contenido);
-  var user = contenido[0].user;
-  var info_user = {src:contenido[0].src,name_libro:contenido[0].name_libro};
-  console.log("info_user");
-  console.log(user);
-  console.log(info_user);
-  //buscamos el usuario para refrescar el contenido de nuestro carri
-  console.log("Old Content:");
-  var value_old = req.cookies[user];
-  console.log(value_old);
-  if(value_old == null){
-    console.log("=== El usuario 0 productos  ===");
-    var list = [];
-    list.push(info_user);
-    res.cookie(user,JSON.stringify(list),{encode: String});
-    res.sendFile(__dirname + '/index.html');
-  }else{
-    console.log("=== El usuario añade mas productos a los antiguos  ===");
-    value_old = JSON.parse(value_old);
-    value_old.push(info_user);
-    console.log("New Content:");
-    console.log(value_old);
-    res.cookie(user,JSON.stringify(value_old),{encode: String});
-    res.sendFile(__dirname + '/index.html');
-  }
+	console.log("====================================");
+	console.log("=== El usuario realiza la compra ===");
+	console.log("====================================");
+	var user = req.body.user;
+	console.log(">>>> lista compra de "+user+":");
+	var carrito=JSON.parse(req.cookies[user]);
+	var compra = carrito.carrito;
+	for(var i=0;i < compra.length;i++){
+		var item  = compra[i].compra;
+		console.log(">> libro:"+item.libro+";Unidades:"+item.cantidad);
+	}
+	console.log("====");
+	res.clearCookie(user);
+	console.log(">> Eliminamos en contenido de la cookie de "+user);
+	res.sendFile(__dirname + '/Index_Respuesta.html');
 });
   
 
